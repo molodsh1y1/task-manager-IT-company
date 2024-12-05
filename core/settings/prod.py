1cc3ae046eacd4a8449d085c172f813bb933adc7
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-12-05 16:30:00 +0200

    Add build script and update production settings

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 # SECURITY WARNING: don"t run with debug turned on in production!
@@ -5,6 +7,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
